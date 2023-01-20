9682eb4ab0862c13d1dcfe342a5c52ea718926f0
Frank-Brice <113094360+Frank-Brice@users.noreply.github.com>
2023-01-20 13:17:33 +0100

    Add files via upload

---
+++ components/couseSideBar.php
@@ -0,0 +1,53 @@
+   <div class="right-sidebar">
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/business.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Business</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/Graphic design.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Graphic Design</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/Hacking.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Hacking</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/video.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Photography</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><img src="../images/couser3.jpg" class="thumbnail" alt=""></a>
+           <div class="video-info">
+               <a>programming</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+
+
+
+   </div>
