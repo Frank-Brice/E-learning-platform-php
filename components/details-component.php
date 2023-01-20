4cf8066ac3e719dbace5e221153840c238009021
Frank-Brice <113094360+Frank-Brice@users.noreply.github.com>
2023-01-20 13:16:05 +0100

    Add files via upload

---
+++ components/details-component.php
@@ -0,0 +1,130 @@
+<?php
+
+
+$id = $_GET['id'];
+require("./db_connection.php");
+$sql = "SELECT * FROM courses WHERE id = $id";
+$result = mysqli_query($connect, $sql);
+// $sideBar = include "couseSideBar.php";
+?>
+<div class="container">
+
+    <div class="row courses" id="maincourselist">
+        <?php
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $courseTitle = $row['course_title'];
+                $courseDescription = $row['course_desc'];
+                $courseRequirements = $row['course_requirements'];
+                $image = $row['course_image'];
+                $video = $row['course_video'];
+                $id = $row['id'];
+                $date = date("d/m/Y");
+
+                echo '<div class="container m-3 play-contain">
+                           <di class="row">
+                              <div class="play-vid">
+                                   <video class="video"  controls>
+                                  <source src="/admin/videos/' . $video . '" type="video/mp4">
+                                   </video> 
+                                   <div class="tags">
+                                      <a href="/category.php?name=Programming" class="try">#Programming</a>
+                                      <a href="/category.php?name=Graphics Design"  class="try">#Graphic Design</a>
+                                      <a href="/category.php?name=Marketing" class="try">#Marketing</a>
+                                   </div>
+                                   <h3>'.$courseTitle.'</h3>
+                                   <h5>'.$courseDescription. '</h5>
+                                   <h5>4.3 <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star-half-stroke"></i> <a href="">(3,561 ratings)</a> 20,965 students</h5>
+                                   <h5>Created by <a href="">Mathurin Wansi, The codex</a></h5>
+                                   <h5><i class="fa-solid fa-circle-exclamation"></i> Last updated '.$date. ' &nbsp; <i class="fa-solid fa-earth-americas"></i> &nbsp; English &nbsp; <i class="fa-solid fa-closed-captioning"></i> &nbsp; English </h5> <br>
+                                <div class="play-info">
+                                    <h4>Requirements</h4>
+                                    <ul>
+                                      <li>&bull; Windows/Macintosh (OSX) Machine</li>
+                                      <li>&bull; Internet Connection</li>
+                                      <li>&bull; ' . $courseRequirements . '</li>
+                                    </ul>
+                                    <h4>Who this course is for:</h4>
+                                    <p>&bull; Even if you have not touched coding before, it would not matter. The easy step-to-step lectures will quickly guide you through everything you will need to know about coding, mainly '. $courseTitle.'. This course is here for you to get accustomed and familiar with '. $courseTitle.' and its syntax. And above all, '. $courseTitle. ' is one of the easiest coding languages to learn, and there is a lot you can do with it.</p>
+                                </div>
+                                <hr>
+                                <br>
+                                <h4><strong>Instructors</strong></h4>
+                                <h5><a href="">Yitemben Frank</a></h5>
+                                <div class="owner"><img src="../images/brice.jpg" alt="" >
+                                  <div class="icons">
+                                    <p><i class="fa-solid fa-star"></i> 3.4 Instructor Rating</p>
+                                    <p><i class="fa-solid fa-graduation-cap"></i> 74,842 Reviews</p>
+                                    <p><i class="fa-solid fa-person"></i><i class="fa-solid fa-person"></i> 20,000 Students</p>
+                                    <p><i class="fa-solid fa-play"></i> 25 Courses</p>
+                                    
+                                  </div>
+                                  
+                                </div>
+                                <br>
+                                <p><span>&nbsp &nbsp &nbsp Yitemben Frank</span> is currently a Junior at IUC logbessou majoring in Software Engineering and Computer Science. He is the CEO and Founder of The Imposition Anonymously, an online educational platform focused on bringing the best programming content to hundreds of thousands of students around the world.</p>
+                                <p>His programming journey began at the age of 10, starting off with simple Python scripts to crawl the weather. Since then, he has helped too many Student since the beginning of his intelligence and is professionally experienced in Game Dev, Programming language and Web Development. Additionaly, he has competed and won in several games around the world including Competition.</p>
+                              </div> 
+                                  <div class="right-sidebar">
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/business.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Business</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/Graphic design.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Graphic Design</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/Hacking.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Hacking</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><video controls>
+                   <source src="../images/video.mp4" type="video/mp4">
+               </video></a>
+           <div class="video-info">
+               <a>Photography</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+       <div class="side-list">
+           <a href="" class="small-size"><img src="../images/couser3.jpg" class="thumbnail" alt=""></a>
+           <div class="video-info">
+               <a>programming</a>
+               <p>The webfix</p>
+               <p>5k views &bull; 1 days</p>
+           </div>
+       </div>
+
+
+
+   </div>
+                           </div>
+                      </div>';
+            }
+        } else {
+            echo "Nothing gotten from database";
+        }
+        ?>
+
+    </div>
+</div>
--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,91 @@
+<body class="body-footer">
+    <footer>
+        <div class="content">
+            <div class="top">
+                <div class="logo-details">
+                    <!-- <i class="fa-brands fa-slack"></i> -->
+                    <img src="../images/nkap-removebg-preview.png" alt="" width="25%" height="80px">
+                    <span class="logo_name" style="font-family: 'Pacifico', cursive;">Nkap-Learning.</span>
+                </div>
+                <div class="media-icons">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+            <div class="link-boxes">
+                <ul class="box">
+                    <li class="link_name">Company</li>
+                    <li><i class="fa-solid fa-chevron-right"></i>
+                        <a href="#">Home</a>
+                    </li>
+                    <li><i class="fa-solid fa-chevron-right"></i>
+                        <a href="#">About Us</a>
+                    </li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Contact Us</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Get Started</a></li>
+                </ul>
+                <ul class="box">
+                    <li class="link_name">Services</li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">App design</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Web design</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Logo design</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Banner design</a></li>
+                </ul>
+                <ul class="box">
+                    <li class="link_name">Account</li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Profile</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">My account</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Preferences</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Purchase</a></li>
+                </ul>
+                <ul class="box">
+                    <li class="link_name">Courses</li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">HTML & CSS</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Javascript</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Music</a></li>
+                    <li><i class="fa-solid fa-chevron-right"></i> <a href="#">Photography</a></li>
+                </ul>
+                <ul class="box input-box">
+                    <li class="link_name">Subscribe</li>
+                    <li><input type="text" placeholder="Enter your email"></li>
+                    <li><input type="button" value="Subscribe"></li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottom-details">
+            <div class="bottom_text">
+                <span class="copyright_text">Copyright &#169; 2022 <a href="">E-Learning.</a>All rights reserved</span>
+                <span class="policy_terms">
+                    <a href="#">privacy policy</a>
+                    <a href="#">terms & condition</a>
+                </span>
+            </div>
+        </div>
+    </footer>
+</body>
+<script src="//code.tidio.co/e4u1hvbyrd6iz02jnbbdvkqxm2przxk2.js" async></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+    integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+    integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async>
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"
+    integrity="sha512-6UofPqm0QupIL0kzS/UIzekR73/luZdC6i/kXDbWnLOJoqwklBK6519iUnShaYceJ0y4FaiPtX/hRnV/X/xlUQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"
+    integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/code.js"></script>
+</body>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js" integrity="sha512-6UofPqm0QupIL0kzS/UIzekR73/luZdC6i/kXDbWnLOJoqwklBK6519iUnShaYceJ0y4FaiPtX/hRnV/X/xlUQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js" integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="/code.js"></script>
-  </body>
 </html>
