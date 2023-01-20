26effec89c0f870c7ac37cd73875e7de9c8dfe51
Frank-Brice <113094360+Frank-Brice@users.noreply.github.com>
2023-01-20 13:12:59 +0100

    Add files via upload

--- category.php
+++ category.php
@@ -1,3 +1,74 @@
-<?php
+<?php 
+session_start();
+$category = $_GET['name'];
+include "./components/navbar.php";
 
-echo $_GET['name'];
+
+
+require("./db_connection.php");
+$sql = "SELECT * FROM courses WHERE category = '$category'";
+$result= mysqli_query($connect,$sql);
+?>
+<div class="container">
+    <div class="row catBanner mt-2">
+        <img src="images/banners/catbanner/<?php echo $category ?>.jpg" alt="<?php echo $category ?>" width=" 100%">
+    </div>
+    <div id="mainCourse" class="row courses my-3 d-flex align-items-center justify-content-center">
+        <?php 
+           if($result){
+             if(mysqli_num_rows($result)>=1){
+                while($row = mysqli_fetch_assoc($result)){
+                    $id = $row['id'];
+                    $courseTitle = $row['course_title'];
+                    $image = $row['course_image'];
+                    $video = $row['course_video'];
+                    if($_SESSION['name'] == 'Mathurin Wansi'){
+                        echo '<div class="col-lg-3 mx-3">
+                        <div class="card" style="width: 18rem;">
+                          <video poster="/admin/images/'.$image.'" height="190px" controls class="card-img-top">
+                            <source src="/admin/videos/'.$video.'" type="video/mp4">
+                          </video>
+                            <div class="card-body">
+                                <div class="row">
+                                    <h5 class="card-title text-center color">'.$courseTitle.'</h5>
+                                    <div class="d-flex align-items-center justify-content-between">
+                                    <a class="text-danger" href="delete.php?id='.$id.'"><i class="fa-solid fa-trash-can"></i></a>
+                                    <a href="details.php?id='.$id.'"><i class="fa-solid fa-eye"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+                    }else{
+                        echo '<div class="col-lg-3 mx-3">
+                        <div class="card" style="width: 18rem;">
+                          <video poster="/admin/images/'.$image.'" height="190px" controls class="card-img-top">
+                            <source src="/admin/videos/'.$video.'" type="video/mp4">
+                          </video>
+                            <div class="card-body">
+                                
+                                    <h5 class="card-title text-center color">'.$courseTitle.'</h5>
+                                    <div class="d-flex align-item-center justify-content-between" name="id">
+                                    <a href="details.php?id='.$id.'"><i class="fa-solid fa-eye"></i></a>
+                                    <a href="details.php?id=' . $id . '"> <i class="fa-solid fa-download text-success"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+                    }
+                   
+                };
+             }else{
+                echo '
+                   <h1 class="text-danger text-center">No course yet for this category</h1>
+                ';
+             }
+           }else{
+               echo "nothing gotten from the database";
+           }
+           
+        ?>
+
+    </div>
+</div>
+<?php include "./components/footer.php"; ?>
